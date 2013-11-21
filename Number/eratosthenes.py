1357d289c2ba009192701f87808f17f7bc7dc7e4
Ben Eggers <ben.eggers36@gmail.com>
2013-11-21 12:41:28 -0800

    began eratosthenes

---
+++ Number/eratosthenes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Author: Ben Eggers
+
+# Finds prime numbers up to a user-generated value using the Sieve of Eratosthenes
+# algorithm. The algorithm starts with an index i = 2. Then, it iteratively adds
+# the current index number to the primes set, marks all multiples of it (in a boolean
+# array) as non-prime, and increments the index to the next unmarked number. Once
+# it reaches sqrt(size), it has marked all non-primes and just iterates through
+# the boolean array to find the remaining primes.
+
+def main():
+
+
+def get_primes():
+
+
+if __name__ == "__main__":
+	main()
