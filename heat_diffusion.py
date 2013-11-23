6ed686cbcdaa07fc195cfb7de3a8cc4b2f0ccf8c
Ben Eggers <ben.eggers36@gmail.com>
2013-11-23 13:32:58 -0800

    did monte carlo and jakobi iteration, but both are prohibitively slow

--- heat_diffusion.py
+++ heat_diffusion.py
@@ -4,6 +4,7 @@
 import math # for math
 import numpy as np # for faster math
 import PIL # for pretty math
+import random # for random math
 
 # This script can be used several ways: the first is to calculate the final
 # heat distribution in a metal plate given user input for parameters using
@@ -16,7 +17,7 @@ def main():
 	gridx = input("X length of grid (px)?: ")
 	gridy = input("Y length of grid (px)?: ")
 	area_count = input("How many areas would you like each edge to be divided\
- into (1 - min(x, y))?: ")
+ into?: ")
 	bounds = [[],[],[],[]] # one array for each edge
 	for x in range(area_count):
 		bounds[0].append(input("Temperature in area %d of top? " % (x+1)))
@@ -50,51 +51,81 @@ def main():
 	print("Do you want to:\n\
   1)  Use Monte Carlo to generate the image\n\
   2)  Use Jakobi iteration to generate the image\n\
-  3)  Do both and compare runtime/number of computations\n\
+  3)  Do both and compare the number of computations\n\
   4)  Use Jakobi iteration to generate a gif (slow, and requires imagemagick)")
-	answer = raw_input("Answer (default is 1): ")
-	if answer is "2":
-		grid = jakobi(grid)
-	elif answer is "3":
+	answer = raw_input("Answer: ")
+	if answer is "3":
 		compare(grid)
-	elif answer is "4":
-		jakobi_gif(grid)
-	else:
-		grid = monte_carlo(grid)
-	generate_map(grid, name)
+	else: # necessary because compare doesn't return a grid or # of computations
+		if answer is "1":
+			grid, computations = monte_carlo(grid)
+		elif answer is "2" or answer is "4":
+			grid, computations = jakobi(grid, (answer is "4"))
+		else:
+			print("Invalid answer. Quitting...")
+		print("%d computations done." % computations)
+		generate_map(grid, name)
 
-def monte_carlo(grid, cmp=False):
+def monte_carlo(grid):
 	"""
 	Finds a close approximation to the stable heat distribution on the grid
 	using n random walks on each point. Parameters are:
 	1) The grid. Boundaries should already be set.
 	Returns:
-	A (grid, (time, computations)-tuple)-tuple.
+	A (grid, computations)-tuple.
 	"""	
 	iterations = input("How many random walks should be done on each point? ")
+	computations = 0
+	for x in range(1, len(grid) - 1): # don't want the boundaries
+		for y in range(1, len(grid[x]) - 1):
+			# Now we're iterating over each point to do a random walk for
+			total = 0
+			for iteration in range(iterations):
+				# random walk 'iterations' times
+				current_pos = np.array([x, y])
+				# while we're not at an edge
+				while current_pos[0] > 0 and current_pos[0] < len(grid) - 1 and\
+						current_pos[1] > 0 and current_pos[1] < len(grid[x]) - 1:
+					direction = np.random.randint(0, 3)
+					computations += 2 # one for the random number, one for the increment
+					if direction is 0:
+						current_pos[0] += 1 # right
+					elif direction is 1:
+						current_pos[1] += 1 # down
+					elif direction is 2:
+						current_pos[0] -= 1 # left
+					else:
+						current_pos[1] -= 1 # up
+				# now add the boundary to total so we can average them later
+				total += grid[current_pos[0]][current_pos[1]]
+				computations += 1
+			# Now we've done some number of random walks and have the total of
+			# the boundaries we hit, so average them and record it.
+			grid[x][y] = 1.0 * total / (iterations * 1.0)
+			computations += 2
+			print("Point (%d, %d) complete!" % (x, y))
+	return (grid, computations)
 
-
-def jakobi(grid, cmp=False):
+def jakobi(grid, gif=False):
 	"""
 	Finds a  the stable heat distribution on the grid using Jakobi iteration.
 	Parameters are:
 	1) The grid. Boundaries should already be set.
 	Returns:
-	A (grid, (time, computations)-tuple)-tuple.
+	A (grid, computations)-tuple.
 	"""
-	pass
-
-def jakobi_gif(grid, delete=True):
-	"""
-	Generates a gif of heat diffusing through the grid (imagemagick required).
-	Parameters are:
-	1) The grid. Boundaries should already be set.
-	2) A boolean telling whether or not to delete the individual frames after
-		creating the gif (True by default).
-	Returns:
-	A (grid, (time, computations)-tuple)-tuple.
-	"""
-	pass
+	computations = 0
+	keep_going = True
+	while keep_going:
+		keep_going = False # will be set to True on changing a value
+		for x in range(1, len(grid) - 1):
+			for y in range(1, len(grid[x]) - 1):
+				new = (grid[x-1][y] + grid[x+1][y] + grid[x][y-1] + grid[x][y+1]) / 4
+				computations += 8
+				if new != grid[x][y]:
+					keep_going = True
+					grid[x][y] = new
+	return (grid, computations)
 
 def jakobi_single_iteration(grid):
 	"""
@@ -102,7 +133,7 @@ def jakobi_single_iteration(grid):
 	Parameters are:
 	1) The grid.
 	Returns:
-	Nothing.
+	A (grid, computations)-tuple.
 	"""
 	pass
 
