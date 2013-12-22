6d810e1a1a231e198e62026a8429c3a197a4b634
Ben Eggers <ben.eggers36@gmail.com>
2013-12-22 14:25:21 -0800

    started radix sort

---
+++ Sort/radix.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2
+# Author: Ben Eggers
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Implementation of radix sort in Python.
+
+def main():
+	base = 500
+	x_points = []
+	y_points = []
+	for x in range(1, 20):
+		arr = [np.random.randint(0, base*x) for k in range(base*x)]
+		start = int(round(time.time() * 1000))
+		arr = radix_sort(arr)
+		end = int(round(time.time() * 1000))
+		x_points.append(len(arr))
+		y_points.append(end - start)
+		print("%d ms required to sort array of length %d using radix sort." 
+			% (end - start, len(arr)))
+	make_plot(x_points, y_points)
+
+def radix_sort(arr):
+
+def make_plot(x_points, y_points):
+	plt.plot(x_points, y_points, 'ro')
+	plt.axis([0, max(x_points), 0, max(y_points)])
+	plt.xlabel('length of array')
+	plt.ylabel('time to sort (milliseconds)')
+	plt.title('Efficiency of radix sort')
+	plt.show()
+
+if __name__ == "__main__":
+	main()
